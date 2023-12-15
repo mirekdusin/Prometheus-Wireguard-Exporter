a6235f835fd2e29b4b4fc589baf2a66c5795dbe1
Dominik Dvořák <32246499+mirekdusin@users.noreply.github.com>
2023-12-15 11:44:01 +0100

    Update metrics_collector.py
    Fix IndexError in extract_metrics_from_wg_output
    
    Resolved an issue where accessing elements in the fields list without checking its length could lead to IndexError. This was particularly problematic for peers that have not completed a handshake. Added length checks before accessing elements in the list to ensure stability. This fix addresses the issue reported in Issue #1.

--- src/metrics_collector.py
+++ src/metrics_collector.py
@@ -49,20 +49,20 @@ class MetricsCollector:
         return output
 
     def extract_metrics_from_wg_output(self, output):
-        peers = {
-            fields[1]: {
-                'interface': fields[0],
-                'public_key': fields[1],
-                'name': self.find_config_file(fields[1]),
-                'endpoint': fields[3].split(':')[0] if fields[3] != '(none)' else '',
-                'allowed_ips': fields[4],
-                'active': 1 if (time.time() - int(fields[5])) <= 125 else 0,
-                'last_handshake': int(fields[5]),
-                'received': int(fields[6]),
-                'sent': int(fields[7])
-            } for fields in (line.split('\t') for line in output.split('\n')[1:])
-        }
-        return list(peers.values())
+       peers = {
+           fields[1]: {
+               'interface': fields[0],
+               'public_key': fields[1],
+               'name': self.find_config_file(fields[1]),
+               'endpoint': fields[3].split(':')[0] if fields[3] != '(none)' else '',
+               'allowed_ips': fields[4],
+               'active': 1 if len(fields) > 5 and (time.time() - int(fields[5])) <= 125 else 0,
+               'last_handshake': int(fields[5]) if len(fields) > 5 else 0,
+               'received': int(fields[6]) if len(fields) > 6 else 0,
+               'sent': int(fields[7]) if len(fields) > 7 else 0
+           } for fields in (line.split('\t') for line in output.split('\n')[1:])
+       }
+       return list(peers.values())
 
     def set_gauge_values(self, peers):
         for peer in peers:
